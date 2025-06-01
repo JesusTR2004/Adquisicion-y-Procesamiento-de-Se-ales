774d29b726bc9d7536a39b740b50151fef70562f
Jesus Tirado <2230265@upv.edu.mx>
2025-05-31 21:41:53 -0600

    Esta es la primer tarea de PDS

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from src.tarea1 import ejecutar_tarea1
+def main(options):
+    if options[1] == "tarea_1":
+        ejecutar_tarea1()
+    elif options[1] == "tarea_2":
+        if len(options) > 2:
+            understanding_freq(options[2])
+        else:
+            print("Da una frecuencia. Ejemplo: python main.py act_2 2")
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) > 1:
+        main(args)
+    else:
+        print("Da un argumento")
+        print("Ejemplo ( run activity 1 ) : python main.py act_1")
+        print("Ejemplo ( run activity 2 ) : python main.py act_2 1")
